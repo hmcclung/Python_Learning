659e7b5370d776db79d8306edf6857d4fc9d463c
Hunter McClung <38441451+hmcclung@users.noreply.github.com>
2024-12-10 08:57:32 -0700

    Update main.py
    Added simple error handling if user enters a number

--- Day 26 - NATO Alphabet Start/NATO-alphabet-start/main.py
+++ Day 26 - NATO Alphabet Start/NATO-alphabet-start/main.py
@@ -2,8 +2,21 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
-user_word = input("What word would you like to convert?:  ").upper()
-name_letters = [phonetic_dict[letter] for letter in user_word]
-print(name_letters)
+
+# Error handling if user inputs a number
+def generate_phonetic():
+    user_word = input("What word would you like to convert?:  ").upper()
+    try:
+        name_letters = [phonetic_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(name_letters)
+
+
+generate_phonetic()
+
+
 
 
